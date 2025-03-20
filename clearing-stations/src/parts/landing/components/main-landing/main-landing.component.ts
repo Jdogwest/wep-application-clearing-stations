cf7c35f62df6ab595f76aeb1a63446c240eb59a8
Jdogwest <jdogwest000@yandex.ru>
2025-03-20 20:10:05 +0300

    header and preview

--- clearing-stations/src/parts/landing/components/main-landing/main-landing.component.ts
+++ clearing-stations/src/parts/landing/components/main-landing/main-landing.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { AboutUsComponent } from '../about-us/about-us.component';
 import { HeaderLandingComponent } from '../header-landing/header-landing.component';
+import { PreviewBlockComponent } from '../preview-block/preview-block.component';
 
 @Component({
   selector: 'app-main-landing',
-  imports: [HeaderLandingComponent, AboutUsComponent],
+  imports: [HeaderLandingComponent, PreviewBlockComponent],
   templateUrl: './main-landing.component.html',
   styleUrl: './main-landing.component.scss',
 })
---
+++ clearing-stations/src/parts/landing/components/preview-block/preview-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-block',
+  imports: [],
+  templateUrl: './preview-block.component.html',
+  styleUrl: './preview-block.component.scss'
+})
+export class PreviewBlockComponent {
+
+}
