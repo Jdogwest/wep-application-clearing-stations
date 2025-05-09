6e0c2a3c17e5b504e2a5b25f2b35e338783cfa03
Jdogwest <jdogwest000@yandex.ru>
2025-05-09 17:46:50 +0300

    about-us and why-us blocks

--- clearing-stations/src/parts/landing/components/main-landing/main-landing.component.ts
+++ clearing-stations/src/parts/landing/components/main-landing/main-landing.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AboutUsComponent } from '../about-us/about-us.component';
 import { HeaderLandingComponent } from '../header-landing/header-landing.component';
 import { PreviewBlockComponent } from '../preview-block/preview-block.component';
+import { WhyUsComponent } from '../why-us/why-us.component';
 
 @Component({
   selector: 'app-main-landing',
-  imports: [HeaderLandingComponent, PreviewBlockComponent],
+  imports: [
+    HeaderLandingComponent,
+    PreviewBlockComponent,
+    AboutUsComponent,
+    WhyUsComponent,
+  ],
   templateUrl: './main-landing.component.html',
   styleUrl: './main-landing.component.scss',
 })
