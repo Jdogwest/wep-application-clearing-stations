94e8fd078c55ced6c60324006ffa7f65826b3bcf
Jdogwest <jdogwest000@yandex.ru>
2025-06-05 16:23:33 +0300

    config change

--- backend/app/config.py
+++ backend/app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     ALLOWED_HEADERS: list[str]
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env.default")
     )
 
 
